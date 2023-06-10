a32d4c455bf3569ccf890df2c8bd3da7ea3246da
njahn88 <nja010@email.latech.edu>
2023-06-10 01:44:22 -0500

    GameInput

--- Assets/Scripts/Controllers/InputController.cs
+++ Assets/Scripts/Controllers/InputController.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using System;
 
-public class InputController : MonoBehaviour
+public class InputController : MonoBehaviour, GameInput.IMovementActions
 {
     public static InputController Instance;
-    //private GameInput _gameInput;
+    public event Action<InputState> OnInputStateChange;
+    public event Action<Vector2> OnMovementInput;
+
+    private GameInput _gameInput;
+    private InputState _gameInputState;
 
     private void Start()
     {
@@ -16,7 +22,45 @@ public class InputController : MonoBehaviour
         else
         {
             Instance = this;
-            //_gameInput = new GameInput();
+            _gameInput = new GameInput();
+            SwitchInput(InputState.Defualt);
+            _gameInput.Movement.SetCallbacks(this);
         }
     }
+    #region InputMethods
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        if(context.phase == InputActionPhase.Performed)
+        {
+            OnMovementInput?.Invoke(context.ReadValue<Vector2>());
+        }
+    }
+    #endregion
+    public void SwitchInput(InputState newInputState)
+    {
+        _gameInputState = newInputState;
+        switch (newInputState)
+        {
+            case InputState.Disable:
+                _gameInput.Movement.Disable();
+                break;
+            case InputState.Defualt:
+                _gameInput.Movement.Enable();
+                break;
+            case InputState.Trial1:
+                _gameInput.Movement.Disable();
+                break;
+        }
+        OnInputStateChange?.Invoke(newInputState);
+    }
 }
+public enum InputState
+{
+    Disable,
+    Defualt,
+    Trial1,
+    Trial2,
+    Trial3,
+    Trial4
+}
+
