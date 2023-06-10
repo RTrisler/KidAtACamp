fcfd3aab9e58bda466a0b5539dff62fb7301c7c0
evangray7373@gmail.com <evangray7373@gmail.com>
2023-06-10 17:19:03 -0500

    More audio infrastructure - fade & music/env passoff

---
+++ Assets/Scripts/Audio/AudioController.cs
@@ -0,0 +1,97 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+public class AudioController : MonoBehaviour
+{
+	[SerializeField]
+	ClipRefs ClipRefs;
+	[SerializeField]
+	DayController DayController;
+	[SerializeField]
+	Camera CurrentCamera;
+	[SerializeField]
+	List<AudioSource> CameraSources;
+
+	[SerializeField]
+	AudioMixer Mixer;
+	[SerializeField]
+	AudioMixerGroup EnvGroup;
+	[SerializeField]
+	AudioMixerGroup MusicGroup;
+
+	AudioSource EnvAmbienceSrc => CameraSources.ElementAt(0);
+	AudioSource MusicSrc => CameraSources.ElementAt(1);
+	AudioSource EnvEventSrc => CameraSources.ElementAt(2);
+
+	public void InitializeCameraAudio()
+	{
+			
+	}
+
+	void Start()
+	{	
+		MusicSrc.outputAudioMixerGroup = MusicGroup;
+		EnvAmbienceSrc.outputAudioMixerGroup = EnvGroup;
+		EnvEventSrc.outputAudioMixerGroup = EnvGroup;
+		StartDay();
+	}
+
+	public void StartDay()
+	{
+		// Daycontroller singleton to differentiate (day counter)
+		Debug.Log("AUDIOCONTROLLER/STARTDAY: CURRENT DAY: " + DayController.Instance._dayCounter);
+		switch (DayController.Instance._dayCounter)
+		{
+			case 1:
+				MusicSrc.clip = ClipRefs.DAYTHEMES.ElementAt(0);
+				MusicSrc.Play();
+				EnvAmbienceSrc.clip = ClipRefs.AMBIRDS.ElementAt(0);
+				StartCoroutine(DelayEnvAmbience(MusicSrc.clip.length - 2.5f, true));
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+		}
+	}
+
+	IEnumerator DelayEnvAmbience(float themeLength, bool fadeIn = false)
+	{
+		Debug.Log("Waiting for theme to finish...");
+		yield return new WaitForSeconds(themeLength);
+		Debug.Log("Theme finished, triggering env ambience...");
+		if (fadeIn)
+		{
+			Debug.Log("Fading in...");
+			StartCoroutine(EnvAmbienceFadeIn(10f, 0.3f));
+		}
+		else
+		{
+			EnvAmbienceSrc.Play();
+		}
+	}
+	
+	IEnumerator EnvAmbienceFadeIn(float duration, float targetVolume)
+	{
+		EnvAmbienceSrc.Play();
+		EnvAmbienceSrc.volume = 0.0001f;
+
+        float currentTime = 0;
+        float start = EnvAmbienceSrc.volume;
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            EnvAmbienceSrc.volume = Mathf.Lerp(start, targetVolume, currentTime / duration);
+			Debug.Log("Env noise volume: " + EnvAmbienceSrc.volume);
+            yield return null;
+        }
+        yield break;
+	}
+}
---
+++ Assets/Scripts/Audio/ClipRefs.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClipRefs : MonoBehaviour
+{
+	public List<AudioClip> AMBIRDS;
+	public List<AudioClip> PMCRICKETS;
+	public List<AudioClip> CAMPFIRES;
+	public List<AudioClip> DAYTHEMES;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
