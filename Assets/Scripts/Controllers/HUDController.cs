5e6dc7f44e4fde88d8d8c1bd2cd6b295e22ca256
njahn88 <nja010@email.latech.edu>
2023-06-11 13:31:46 -0500

    Added HUD for pickups

--- Assets/Scripts/Controllers/HUDController.cs
+++ Assets/Scripts/Controllers/HUDController.cs
@@ -6,6 +6,9 @@ using TMPro;
 public class HUDController : MonoBehaviour
 {
     public GameObject interactionPrompt;
+    public GameObject pickUpCounter;
+    private int _pickUpCount;
+    private int _pickUpTotal;
     public static HUDController Instance;
 
     void Awake()
@@ -13,11 +16,41 @@ public class HUDController : MonoBehaviour
         Instance = this;
 
         ChangeInteractText("Click to move...");
+        HidePickUp();
+        DayController.Instance.OnPickUpTask += ShowPickUp;
+        DayController.Instance.OnAllItemsCollected += HidePickUp;
+        GuidedTaskPickUp.OnGuidedTaskPickUp += IncreaseCount;
+    }
+
+    private void OnDisable()
+    {
+        DayController.Instance.OnPickUpTask -= ShowPickUp;
+        GuidedTaskPickUp.OnGuidedTaskPickUp -= IncreaseCount;
+        DayController.Instance.OnAllItemsCollected -= HidePickUp;
     }
 
     public void ShowInteractionTip()
     {
         interactionPrompt.SetActive(true);
+    }
+
+    public void ShowPickUp(int pickUpTotal)
+    {
+        _pickUpTotal = pickUpTotal;
+        _pickUpCount = 0;
+        pickUpCounter.SetActive(true);
+        pickUpCounter.GetComponent<TextMeshProUGUI>().text = _pickUpCount + "/" + _pickUpTotal;
+    }
+
+    public void IncreaseCount(GuidedTaskInteractable guidedTask)
+    {
+        _pickUpCount++;
+        pickUpCounter.GetComponent<TextMeshProUGUI>().text = _pickUpCount + "/" + _pickUpTotal;
+    }
+
+    public void HidePickUp()
+    {
+        pickUpCounter.SetActive(false);
     }
 
     public void HideInteractionTip()
